15740e69e55f400e8de0d51d486efaff09f0a57d
FernandoFigueroa14 <luisfersurfer14@hotmail.com>
2021-06-28 12:01:06 -0500

    Creacion del proyecto en NodeJs, header y footer.

---
+++ Code/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/static', express.static(path.join(__dirname,'/public')));
+
+app.get('/' ,function(req, res){
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/home.html' ,function(req, res){
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.listen(3000, ()=>{
+    console.log('Servidor activo en el puerto 3000');
+});
