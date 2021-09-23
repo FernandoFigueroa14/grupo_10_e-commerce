adda41bf4423c7001e5c79b5687e3d9488ab0f96
fede-veloper <fedev.code@gmail.com>
2021-09-22 20:01:24 -0500

    db-conection-working

--- Code/controllers/categoriesController.js
+++ Code/controllers/categoriesController.js
@@ -4,25 +4,25 @@ const { Op } = require('sequelize')
 
 let db = require('../database/models')
 
-// const productsJSON = path.resolve('./data/dataProducts.json')
-// const products = JSON.parse(fs.readFileSync(productsJSON, 'utf-8'))
+/* const productsJSON = path.resolve('./data/dataProducts.json')
+const products = JSON.parse(fs.readFileSync(productsJSON, 'utf-8')) */
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 
 const categoriesController = {
   women: (req, res, next) => {
     RenderCategory('female', 'women', res, next)
-    // const womenProducts = products.filter(product => product.category === 'female')
-    // res.render(path.resolve('views/categories/women'), { products: womenProducts, toThousand: toThousand })
+    /* const womenProducts = products.filter(product => product.category === 'female')
+    res.render(path.resolve('views/categories/women'), { products: womenProducts, toThousand: toThousand }) */
   },
   men: (req, res, next) => {
     RenderCategory('male', 'men', res, next)
-    // const menProducts = products.filter(product => product.category === 'male')
-    // res.render(path.resolve('views/categories/men'), { products: menProducts, toThousand: toThousand })
+    /* const menProducts = products.filter(product => product.category === 'male')
+    res.render(path.resolve('views/categories/men'), { products: menProducts, toThousand: toThousand }) */
   },
   kids: (req, res, next) => {
     RenderCategory('kid', 'kids', res, next)
-    // const kidsProducts = products.filter(product => product.category === 'kid')
-    // res.render(path.resolve('views/categories/kids'), { products: kidsProducts, toThousand: toThousand })
+    /* const kidsProducts = products.filter(product => product.category === 'kid')
+    res.render(path.resolve('views/categories/kids'), { products: kidsProducts, toThousand: toThousand }) */
   }
 }
 
