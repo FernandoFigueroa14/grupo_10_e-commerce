99467564b821e86e9132dc60460c63cd855de7bb
fede-veloper <fedev.code@gmail.com>
2021-09-25 09:48:41 -0500

    chekc-conection-db

--- Code/controllers/mainController.js
+++ Code/controllers/mainController.js
@@ -1,20 +1,18 @@
 const path = require('path')
-// const fs = require('fs')
 
 const db = require('../database/models')
-// const productsJSON = path.resolve('./data/dataProducts.json')
-// const products = JSON.parse(fs.readFileSync(productsJSON, 'utf-8'))
+
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 
 const controller = {
-  home: (req, res, next) => {
-    db.Products.findAll()
-      .then((productsDB) => {
-        // console.log(productsDB)
-        res.render(path.resolve('views/home'), { products: productsDB, toThousand: toThousand })
-      }).catch((error) => {
-        next(error)
-      })
+  home: async (req, res) => {
+    let productsDB = []
+    try {
+      productsDB = await db.Products.findAll()
+    } catch (error) {
+      console.log(error.message)
+    }
+    res.render(path.resolve('views/home'), { products: productsDB, toThousand: toThousand })
   }
 }
 
