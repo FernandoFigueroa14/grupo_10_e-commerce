e09bb054462b477ae9fecc7cff96965127efc68f
Brayan63 <brayan-garcia12345@hotmail.com>
2021-09-24 19:45:55 -0500

    dda

---
+++ Code/database/config/config.js
@@ -0,0 +1,25 @@
+module.exports= {
+    'development': {
+      'username': 'root',
+      'password': "perro123",
+      'database': 'dh_db',
+      'host': '127.0.0.1',
+      'dialect': 'mysql',
+      'port': '4550'
+    },
+    'test': {
+      'username': 'root',
+      'password': null,
+      'database': 'dh-clothes',
+      'host': '127.0.0.1',
+      'dialect': 'mysql'
+    },
+    'production': {
+      'username': 'root',
+      'password': null,
+      'database': 'dh-clothes',
+      'host': '127.0.0.1',
+      'dialect': 'mysql'
+    }
+  }
+  
