5c27524f522e6f826c8875aacd8e29af7b676ccd
Brayan63 <brayan-garcia12345@hotmail.com>
2021-10-14 01:39:07 -0500

    validacion

---
+++ Code/public/js/validations/registerValidations.js
@@ -0,0 +1,10 @@
+Window.addEventListener("load",function() {
+    const email = document.querySelector("#email")
+    const pass = document.querySelector("#password")
+    const name = document.querySelector("#name")
+    const firstName = document.querySelector("#firstName")
+    const picture = document.querySelector("#picture")
+    const birthDate = document.querySelector("#birthDate") 
+    const genre = document.querySelector("#genre")
+    
+})
