f2fb67e9e6f23cbd166c8b0dfd8d16e2781c6ae2
Brayan63 <brayan-garcia12345@hotmail.com>
2021-10-15 18:51:37 -0500

    validations

--- Code/public/js/validations/registerValidations.js
+++ Code/public/js/validations/registerValidations.js
@@ -1,10 +1,40 @@
 Window.addEventListener("load",function() {
-    const email = document.querySelector("#email")
-    const pass = document.querySelector("#password")
-    const name = document.querySelector("#name")
-    const firstName = document.querySelector("#firstName")
-    const picture = document.querySelector("#picture")
-    const birthDate = document.querySelector("#birthDate") 
-    const genre = document.querySelector("#genre")
-    
+    const form = document.querySelector("form-control-register")
+   
+form.addEventListener(submit, function (e){
+    e.preventDefault()
+     
+    let  email = document.querySelector("#e-mail")
+    if (email.value == ""){
+        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+    } 
+    if
+    let  pass = document.querySelector("#password")
+    if (pass.value == ""){
+        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+    }
+    let  name = document.querySelector("#name")
+    if (name.value ==""){
+        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+    } else if (name.value.length<2){ 
+      alert ("Te faltan letras del nombre")
+      }
+    let  firstName = document.querySelector("#firstName")
+    if (firstName.value == ""){
+        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+    }
+
+    let  picture = document.querySelector("#picture")
+    if (pass.value == ""){
+        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+    }
+    let  birthDate = document.querySelector("#birthDate") 
+    if (pass.value == ""){
+        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+    }
+    let  genre = document.querySelector("#genre")
+    if (pass.value == ""){
+        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+    }
+})
 })
