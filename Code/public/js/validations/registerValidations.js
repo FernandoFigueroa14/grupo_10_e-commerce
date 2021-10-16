9343b2b375a64e39a243560a481acc51625f9c9d
Brayan63 <brayan-garcia12345@hotmail.com>
2021-10-15 19:12:33 -0500

    val

--- Code/public/js/validations/registerValidations.js
+++ Code/public/js/validations/registerValidations.js
@@ -2,39 +2,54 @@ Window.addEventListener("load",function() {
     const form = document.querySelector("form-control-register")
    
 form.addEventListener(submit, function (e){
-    e.preventDefault()
+    e.preventDefault();
+
+    let errores = [];
      
     let  email = document.querySelector("#e-mail")
     if (email.value == ""){
-        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+        errores.push ("¿Olvidas algo? ¿o te lo recuerdo?")
     } 
-    if
     let  pass = document.querySelector("#password")
     if (pass.value == ""){
-        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+        errores.push ("¿Olvidas algo? ¿o te lo recuerdo?")
+    }else if (pass.value.length<8){ 
+        errores.push ("Te faltan letras de la contraseña")
     }
     let  name = document.querySelector("#name")
     if (name.value ==""){
-        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+        errores.push ("¿Olvidas algo? ¿o te lo recuerdo?")
     } else if (name.value.length<2){ 
-      alert ("Te faltan letras del nombre")
+        errores.push ("Te faltan letras del nombre")
       }
     let  firstName = document.querySelector("#firstName")
     if (firstName.value == ""){
-        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
-    }
+        errores.push ("¿Olvidas algo? ¿o te lo recuerdo?")
+    }else if (firstName.value.length<2){ 
+        errores.push ("Te faltan letras del apellido")
 
     let  picture = document.querySelector("#picture")
     if (picture.value == ""){
-        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+        errores.push ("¿Olvidas algo? ¿o te lo recuerdo?")
     }
     let  birthDate = document.querySelector("#birthDate") 
     if (birthDate.value == ""){
-        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+        errores.push ("¿Olvidas algo? ¿o te lo recuerdo?")
     }
     let  genre = document.querySelector("#genre")
     if (genre.value == ""){
-        alert ("¿Olvidas algo? ¿o te lo recuerdo?")
+        errores.push ("¿Olvidas algo? ¿o te lo recuerdo?")
+    }
+    let ulErrores = document.querySelector("div.errores ul")
+    if (errores.length < 0){ 
+        e.preventDefault();
+        for (let index = 0; index < errores.length; index++){
+           ulErrores.innerHTML += "<li>" + errores[li] + "</li>"            
+        }
+
+            
+        
     }
 })
 })
+
