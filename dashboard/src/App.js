e4fb26ec6703d77fb1e09118dccdd1e46e705d7a
fede-veloper <fedev.code@gmail.com>
2021-10-20 20:21:04 -0500

    users-products

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,6 +1,17 @@
-import './App.css';
+import React, { useEffect } from 'react' 
+import axios from 'axios'
 
 function App() {
+  useEffect(() => {
+    const getDataProducts = async () => {
+      const responseProducts = await axios.get('/api/products')
+      const responseUsers = await axios.get('/api/users')
+      console.log(responseProducts.data.data)
+      console.log(responseUsers.data.data)
+    }
+    getDataProducts()
+  }, [])
+
   return (
     <p>React</p>
   );
